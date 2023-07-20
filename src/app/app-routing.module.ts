6929dd4c9535e58c1d06c837fca99f58c206cc66
SBellido <sebastianbellidodg@gmail.com>
2023-07-20 14:21:15 +0200

    works & errors components was created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorsComponent } from './errors/errors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'works',
+        loadChildren: () => import('./works/works.module')
+          .then(m => m.WorksModule)
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'works/home' 
+      },
+      {
+        path: '**',
+        component: ErrorsComponent
+      }
+    ]
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorsComponent } from './errors/errors.component';
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
